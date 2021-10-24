2df97a7ff88e45611dd752841ac9e49de5c93b1e
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-10-24 10:05:47 +0800

    Chakra UI & Hardhat init

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "@fontsource/manrope";
+import "@fontsource/poppins";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Manrope",
+    body: "Poppins",
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
