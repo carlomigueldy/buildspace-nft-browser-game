18ec27e60c47be77b70a9a082ed8641164fd3b37
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-10-24 10:31:55 +0800

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import { Box, Center, Text } from "@chakra-ui/layout";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-
   return (
-    <> </>
-  )
+    <Box
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Center>
+        <Box>
+          <Text fontSize="4xl">Ethereum's Awesome NFT Game</Text>
+          <p>Test</p>
+        </Box>
+      </Center>
+    </Box>
+  );
 }
 
-export default App
+export default App;
